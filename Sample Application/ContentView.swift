13115e280bc5f958e2af51381f8d0d267297645c
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-04-20 01:49:53 +0200

    feat: Implementing FAQs in your applications (completed Dynamic Application FAQs in SwiftUI)

--- Sample Application/ContentView.swift
+++ Sample Application/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showFAQ = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+            .navigationTitle("Sample Application")
+            .toolbar {
+                Button {
+                    showFAQ.toggle()
+                } label: {
+                    Image(systemName: "info.circle.fill")
+                }
+            }
+            .sheet(isPresented: $showFAQ) {
+                FAQView()
+            }
         }
-        .padding()
     }
 }
 
